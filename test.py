2b12570bc6fb1c8d044b890d28ff1d3a271e948f
kitikitikiti15 <46821562+kitikitikiti15@users.noreply.github.com>
2019-10-22 19:11:06 -0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,8 @@
+import analysis
+def test_average():
+    avg = analysis.average_inflammations(0)
+assert(avg == 5.45)
+def test_max():
+raise Exception("Remove this line and implement your test instead")
+def test_acute_patient():
+raise Exception("Remove this line and implement your test instead")
